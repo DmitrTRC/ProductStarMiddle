a7881ea4e0c1407ee2c2eb97c17c9993c138dce1
Dmitry Morozov <9984398@gmail.com>
2023-05-15 02:21:28 +0300

    --> Tests: Done

---
+++ Homeworks/Test_SquareRootEquation.py
@@ -0,0 +1,21 @@
+import unittest
+import SquareRootEquation
+
+
+class GetRootsTestCase(unittest.TestCase):
+    def test_get_discriminant(self):
+        self.assertEqual(SquareRootEquation.get_discriminant(1, 2, 1), 0)
+        self.assertEqual(SquareRootEquation.get_discriminant(1, 2, 3), -8)
+        self.assertEqual(SquareRootEquation.get_discriminant(1, 2, 4), -12)
+
+    def test_get_roots(self):
+        self.assertEqual(SquareRootEquation.get_roots(1, 2, 1), [-1])
+        self.assertEqual(SquareRootEquation.get_roots(1, 2, 3), [])
+        self.assertEqual(SquareRootEquation.get_roots(2, 3, -5), [1, -2.5])
+
+        self.assertNotEquals(SquareRootEquation.get_roots(1, 2, 1), [1])
+        self.assertNotEquals(SquareRootEquation.get_roots(1, 2, 3), [1, 2])
+
+
+if __name__ == '__main__':
+    unittest.main()
